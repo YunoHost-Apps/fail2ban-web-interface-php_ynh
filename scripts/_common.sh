97f4afd26f0c4421e142f997b40a127968f75883
Sylvain <syl@thinkpad.localhost>
2024-09-22 10:15:17 +0200

    Update

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,14 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
+fail2ban_sock="/var/run/fail2ban/fail2ban.sock"
+
 myynh_set_permissions () {
 	chown -R "$app:www-data" "$install_dir"
 	chmod 750 "$install_dir"
 	chmod -R o-rwx "$install_dir"
 
 	chown root: "/etc/sudoers.d/$app"
+
+	chmod +x "/usr/local/bin/$app"
 }
